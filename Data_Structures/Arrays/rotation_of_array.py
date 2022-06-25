7d0aedae71a06bccd10022f2ae669fd67a352615
mchawla5 <mayankch@usc.edu>
2022-06-25 15:23:54 -0400

    Method 3 added which uses reversal algorithm

--- Data_Structures/Arrays/rotation_of_array.py
+++ Data_Structures/Arrays/rotation_of_array.py
@@ -12,6 +12,9 @@ SOLUTION:
 - Method 2
     |_ Time Complexity = O(n)
     |_ Auxiliary Space = O(1)
+- Method 3
+    |_ Time Complexity = O(n)
+    |_ Auxiliary Space = O(1)
 '''
 from sys import argv
 import time
@@ -61,6 +64,26 @@ def rotateArrayM2(arr, elements_to_rotate):
     print(arr)
 
 
+# Method 3 (reversal algorithm):
+def reverseArray(arr, start, end):
+    while (start < end):
+        temp = arr[start]
+        arr[start] = arr[end]
+        arr[end] = temp
+        start += 1
+        end -= 1
+
+def rotateArrayM3(arr, elements_to_rotate):
+    if elements_to_rotate == 0:
+        return
+    n = len(arr)
+    d = elements_to_rotate % n
+    reverseArray(arr, 0, d-1)
+    reverseArray(arr, d, n-1)
+    reverseArray(arr, 0, n-1)
+    print(arr)
+
+
 # Main code
 if __name__ == "__main__":
     if (len(argv) > 1):
@@ -69,5 +92,7 @@ if __name__ == "__main__":
             rotateArrayM1(arr, elements_to_rotate)
         elif (argv[1] == ('m2' or 'M2')):
             rotateArrayM2(arr, elements_to_rotate)
+        elif (argv[1] == ('m3' or 'M3')):
+            rotateArrayM3(arr, elements_to_rotate)
     else:
         print("\nERROR: Please select an argument between 'm1/M1' or 'm2/M2' to run the code with the correspodning method!\n")
