143befd57078fdd14dae2463bce8507ea994d807
mchawla5 <mayankch@usc.edu>
2022-06-25 15:07:23 -0400

    modified the two methods

--- Data_Structures/Arrays/rotation_of_array.py
+++ Data_Structures/Arrays/rotation_of_array.py
@@ -1,7 +1,7 @@
 '''
 ROTATION OF ARRAY
 
-The problem requires us to have function to rotate an array 'arr' of size 'n' by a number of elements 'd'
+The problem requires us to have a function to rotate an array 'arr' of size 'n' by a number of elements 'd'
 
 Eg: arr = [1,2,3,4,5,6] if rotated by d=3, output = [4,5,6,1,2,3]
 
@@ -18,35 +18,35 @@ import time
 
 
 # Input method (asking user for inputs):
-def _ask_input():
+def askInput():
     size = int(input("Enter the size of array: "))
     arr = []
     for i in range(size):
         arr.append(int(input("Enter the value into array: ")))
-    print("\narr[{}] = {}".format(size, arr))
+    print("\narr[] = {}".format(arr))
     elements_to_rotate = int(input("\nEnter the elements you want to rotate: "))
     return arr, elements_to_rotate
 
 
 # Method 1 (using a temp array):
-def _rotate_array_m1(arr, elements_to_rotate):
+def rotateArrayM1(arr, elements_to_rotate):
     new_arr = []
     new_arr = arr[elements_to_rotate:] + arr[0:elements_to_rotate]
     print(new_arr)
 
 
 # Method 2 (using a GCD value):
-def _gcd(a,b):
+def gcd(a,b):
     if (b == 0):
         return a
     else:
-        return _gcd(b, a%b)
+        return gcd(b, a%b)
 
-def _rotate_array_m2(arr, elements_to_rotate):
+def rotateArrayM2(arr, elements_to_rotate):
     n = len(arr)
     d = elements_to_rotate % n
-    gcd = _gcd(d,n)
-    for i in range(gcd):
+    gcd_value = gcd(d,n)
+    for i in range(gcd_value):
         temp = arr[i]
         j = i
         while 1:
@@ -64,10 +64,10 @@ def _rotate_array_m2(arr, elements_to_rotate):
 # Main code
 if __name__ == "__main__":
     if (len(argv) > 1):
-        arr, elements_to_rotate = _ask_input()
+        arr, elements_to_rotate = askInput()
         if (argv[1] == ('m1' or 'M1')):
-            _rotate_array_m1(arr, elements_to_rotate)
+            rotateArrayM1(arr, elements_to_rotate)
         elif (argv[1] == ('m2' or 'M2')):
-            _rotate_array_m2(arr, elements_to_rotate)
+            rotateArrayM2(arr, elements_to_rotate)
     else:
-        print("Please select an argument between 'm1/M1' or 'm2/M2' to select the method to run the code!")
+        print("\nERROR: Please select an argument between 'm1/M1' or 'm2/M2' to run the code with the correspodning method!\n")
