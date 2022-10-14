738701152a5ea5f5209b6876aecb152d034c9c8b
mchawla5 <mayankch@usc.edu>
2022-10-13 22:26:28 -0400

    method 2 added

--- Data_Structures/Binary_Tree/Traversals/postorder_from_in_and_pre.py
+++ Data_Structures/Binary_Tree/Traversals/postorder_from_in_and_pre.py
@@ -26,6 +26,21 @@ def postOrderM1(inorder_arr, preorder_arr, n):
         postOrderM1(inorder_arr[root_index+1:n], preorder_arr[root_index+1:n], n-root_index-1)
     print(preorder_arr[0], end=" ")
 
+def postOrderM2_call(inorder_arr, preorder_arr, in_start, in_end):
+    global pre_index, hm
+    if (in_start > in_end):
+        return
+    in_index = hm[preorder_arr[pre_index]]
+    pre_index += 1
+    postOrderM2_call(inorder_arr, preorder_arr, in_start, in_index-1)
+    postOrderM2_call(inorder_arr, preorder_arr, in_index+1, in_end)
+    print(inorder_arr[in_index], end=" ")
+
+def postOrderM2(inorder_arr, preorder_arr, n):
+    for i in range(n):
+        hm[inorder_arr[i]] = i
+    postOrderM2_call(inorder_arr, preorder_arr, 0, n-1)
+
 if __name__=='__main__':
     inorder_array = [4,2,5,1,3,6]
     preorder_array = [1,2,4,5,3,6]
@@ -34,3 +49,10 @@ if __name__=='__main__':
     if(method == 1):
         print("Postorder traversal: ")
         postOrderM1(inorder_array, preorder_array, n)
+    elif(method == 2):
+        hm = {}
+        pre_index = 0
+        print("Postorder traversal: ")
+        postOrderM2(inorder_array, preorder_array, n)
+    else:
+        print("No valid method was selected\n")
